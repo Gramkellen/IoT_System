713356ca4ab451bc141890fee2d7107d1ae1af7c
Su-JT <sjt_12@qq.com>
2023-12-30 01:51:53 +0800

    更新订阅
    连接mqtt,模拟接口，调整样式

--- src/mock/index.js
+++ src/mock/index.js
@@ -4,8 +4,17 @@ import Mock from 'mockjs'
 // let useMock = true
 let useMock = true;
 if (useMock) {
-   
-  
+    Mock.mock('/weathersystem/subscribe/', 'post',(options) =>{         //三个主题订阅状态
+        console.log(options.body);
+        return{
+        };
+    })
+
+    Mock.mock('/weathersystem/history/', 'post',(options) =>{         //保存历史记录
+        console.log(options.body);
+        return{
+        };
+    })
 
 }
 
